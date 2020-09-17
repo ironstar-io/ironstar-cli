bea78746c7b50371813864c2f0b25771097f8763
Jimmy Cann <jimmy.cann@ironstar.io>
2020-09-17 16:16:04 +1000

    Add ETA to new backup output

--- internal/backup/new.go
+++ internal/backup/new.go
@@ -53,14 +53,14 @@ func New(args []string, flg flags.Accumulator) error {
 		fmt.Println("- " + comp)
 	}
 
-	fmt.Println()
-
 	if br.ETA != 0 {
-		fmt.Println("This backup will take approximately " + strconv.Itoa(br.ETA) + " minutes (based on previous similar backups) to complete")
+		fETA := CalucateFriendlyETA(br.ETA)
+		fmt.Println()
+		fmt.Println("This backup will take approximately " + fETA + "(based on previous similar backups) to complete")
 	}
 
 	fmt.Println()
-	fmt.Println("You can check the status at any time by running `iron backup status`")
+	fmt.Println("You can check the status at any time by running `iron backup info " + br.Name + "`")
 	fmt.Println()
 
 	color.Green("Successfully commenced backup of the environment '" + seCtx.Environment.Name + "'")
@@ -82,4 +82,39 @@ func CalculateBackupRequestKind(ogKind string) string {
 	}
 
 	return "manual"
+}
+
+func CalucateFriendlyETA(eta int) string {
+	if eta <= 60 {
+		return strconv.Itoa(eta) + " seconds "
+	}
+
+	if eta <= 3600 {
+		minutes := eta / 60
+		seconds := eta % 60
+
+		var minStr = " minute "
+		if minutes > 1 {
+			minStr = " minutes "
+		}
+		var secStr = " second "
+		if seconds > 1 {
+			secStr = " seconds "
+		}
+		return strconv.Itoa(minutes) + minStr + strconv.Itoa(seconds) + secStr
+	}
+
+	minutes := eta / 60
+	hours := minutes / 60
+	rMins := minutes % 60
+
+	var hrStr = " hour "
+	if hours > 1 {
+		hrStr = " hours "
+	}
+	var minStr = " minute "
+	if rMins > 1 {
+		minStr = " minutes "
+	}
+	return strconv.Itoa(hours) + hrStr + strconv.Itoa(minutes) + minStr
 }
