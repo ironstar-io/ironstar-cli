30f4c595c990a7e7abe0a725f57e4f6abe9ddec3
Jimmy Cann <jimmy.cann@ironstar.io>
2020-11-25 17:00:05 +1100

    Add a warning before package upload

--- internal/api/upload.go
+++ internal/api/upload.go
@@ -6,11 +6,19 @@ import (
 	"gitlab.com/ironstar-io/ironstar-cli/internal/system/fs"
 	"gitlab.com/ironstar-io/ironstar-cli/internal/types"
 
+	"github.com/fatih/color"
 	"github.com/pkg/errors"
 )
 
 // UploadPackage - Create a project tarball in tmp
 func UploadPackage(creds types.Keylink, subHash, tarpath, ref string) (*RawResponse, error) {
+	color.Red(`Warning! This command uploads the contents of this Git repository to Ironstar and makes it available on the web. Only paths listed under the "exclude" settings in your .ironstar/config.yml file will be excluded.
+
+This means that any database files, Drupal Public and Private Files, and other potentially sensitive content that you have in this repository that is not in the exclude list will be uploaded to the remote environment. 
+
+Please proceed with caution. 
+	`)
+
 	wo := console.SpinStart("Uploading package file to Ironstar system")
 
 	req := &Stream{
