dbe467951e709d1de4d3ac5fb124c73b8219b54f
Jimmy Cann <jimmy.cann@ironstar.io>
2021-03-18 10:02:34 +1100

    NANKAI-147: Display expires_after for backup info if available

--- internal/types/main.go
+++ internal/types/main.go
@@ -140,12 +140,13 @@ type CacheInvalidation struct {
 }
 
 type BackupRequest struct {
-	Name       string    `json:"name,omitempty" yaml:"name,omitempty"`
-	Kind       string    `json:"kind,omitempty" yaml:"kind,omitempty"`
-	Schedule   string    `json:"schedule,omitempty" yaml:"schedule,omitempty"`
-	ETA        int       `json:"eta,omitempty" yaml:"eta,omitempty"`
-	Components []string  `json:"components,omitempty" yaml:"components,omitempty"`
-	CreatedAt  time.Time `json:"created_at,omitempty" yaml:"created_at,omitempty"`
+	Name         string    `json:"name,omitempty" yaml:"name,omitempty"`
+	Kind         string    `json:"kind,omitempty" yaml:"kind,omitempty"`
+	Schedule     string    `json:"schedule,omitempty" yaml:"schedule,omitempty"`
+	ETA          int       `json:"eta,omitempty" yaml:"eta,omitempty"`
+	Components   []string  `json:"components,omitempty" yaml:"components,omitempty"`
+	CreatedAt    time.Time `json:"created_at,omitempty" yaml:"created_at,omitempty"`
+	ExpiresAfter time.Time `json:"expires_after" yaml:"expires_after"`
 }
 
 type BackupIterationFlat struct {
